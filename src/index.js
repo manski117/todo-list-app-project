ceccb10bb9df67c29fa350ca1d4d9e22b87c9807
manski117 <choromanski117@gmail.com>
2022-12-21 13:25:07 -0500

    Add icons to index.html
    All major functionallity successfully working and bundled via
    webpack.

--- src/index.js
+++ src/index.js
@@ -1,26 +1,6 @@
 import _ from 'lodash';
 import './style.css';
 
-function component() {
-  const element = document.createElement('div');
-
-  // lodash is now imported
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-
-  console.log('console is working');
-  console.log('console is working even after reverting to old config');
-  
-  
-
-  return element;
-}
-
-document.body.appendChild(component());
-
-
-
-
 ///////global vars
 let selectedProject = undefined;
 let currentTask = undefined;
